7ff2068382e81a7da0304015b25b3262dd2bedbe
Acho rauche helvich-destruques boni <rauche@live.fr>
2021-04-01 16:02:41 +0000

    first version of boilerplate

---
+++ postcss.config.js
@@ -0,0 +1,17 @@
+// postcss.config.js
+const purgecss = require('@fullhuman/postcss-purgecss')
+const cssnano  = require('cssnano')
+
+module.exports = {
+  plugins:[
+    require('tailwindcss'),
+    require('autoprefixer'),
+    cssnano({
+      preset: 'default'
+    }),
+    //purgecss({
+      //content: ["./src/templates/**/*.html"],
+      //defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+    //})
+  ]
+}
